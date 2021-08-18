f311f085fb22445ce4e0c3b740dfbfd5ebf13173
LF236 <fenandorodiguez236@gmail.com>
2021-08-17 21:00:28 -0500

    Preparación del home

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3030;
 
-//Rutas
+//Route public
+const public_path = path.resolve(__dirname + '/public');
+app.use(express.static(public_path));
+
+//Routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    const routeHome = path.resolve(__dirname + '/views/home.html');
+    res.sendFile(routeHome);
 });
 
-//Servidor en escucha
+//Listening Server
 app.listen(port, () => {
     console.log(`Servidor en http://localhost:${port}`);
 });
