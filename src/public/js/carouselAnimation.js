d6c979423509111fc71c882d4a36bc166cc71569
LF236 <fenandorodiguez236@gmail.com>
2021-08-31 14:49:40 -0500

    Carousel of the home is done

---
+++ src/public/js/carouselAnimation.js
@@ -0,0 +1,42 @@
+const contentImages = document.getElementById('carousel-content-images');
+let i = 1;
+
+//Change of the color in the icons
+const pivotingIconsOfCarousel = () => {
+    i++;
+    console.log(i);
+    
+    
+    if(i < 6) {
+        const idElementActual = `images-i-element-${i}`;
+        const auxElementActual = document.getElementById(idElementActual);
+        auxElementActual.style = 'color: yellow';
+        const idElementAnterior = `images-i-element-${i - 1}`;
+        const auxElementAnterior = document.getElementById(idElementAnterior);
+        auxElementAnterior.style = 'color: #fff';
+    }
+
+    if(i === 6) {
+        document.getElementById('images-i-element-5').style = 'color: #fff';
+        document.getElementById('images-i-element-1').style = 'color: #yellow';
+        i = 1;
+    }
+}
+
+//Moving of the carousel
+setInterval(() => {
+    let positionNow = getComputedStyle(contentImages).getPropertyValue('--positionNow');
+    positionNow = positionNow.slice(0, -1);
+    let aux = parseInt(positionNow);
+    aux -= 100;
+    if (aux >= -400) {
+        contentImages.style.setProperty('--positionNow', `${aux}%`);
+        
+    }
+
+    if (aux === -500) {
+        aux = 0;
+        contentImages.style.setProperty('--positionNow', `${aux}%`);
+    }
+    pivotingIconsOfCarousel();
+}, 5000);
