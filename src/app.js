673b730e60b51f8c826e9ceb14efc32a2ff2d982
LF236 <fenandorodiguez236@gmail.com>
2021-09-02 08:39:15 -0500

    The files in the folder partials is ready, the documents of header, footer and head is complete for the use in all project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3030;
+const port = 3000;
 
 //Route public
 const public_path = path.resolve(__dirname + '/public');
