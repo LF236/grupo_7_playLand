2eb5bca30f313195fe484a8efcf49e93f3812bf0
LF236 <fenandorodiguez236@gmail.com>
2021-09-09 00:46:43 -0500

    The heade for the movil version is complete

--- src/public/js/menu_animation.js
+++ src/public/js/menu_animation.js
@@ -1,6 +1,11 @@
 const menu_button = document.getElementById('menu_button');
 const menu = document.getElementById('menu');
+const iconX = document.getElementById('icon-x');
 
 menu_button.addEventListener('click', () => {
-    menu.classList.toggle('menu_animate');    
+    menu.classList.add('menu_animate');    
+});
+
+iconX.addEventListener('click', () => {
+    menu.classList.remove('menu_animate');
 });
