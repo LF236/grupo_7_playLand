51f675d9308c7af126ebd2350f699f746cf12b63
LF236 <fenandorodiguez236@gmail.com>
2021-09-20 00:36:30 -0500

    Search Product view is finished, in the desktop and movil version

--- src/app.js
+++ src/app.js
@@ -3,18 +3,23 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-//Route public
+// Route public
 const public_path = path.resolve(__dirname + '/public');
 app.use(express.static(public_path));
 
-//File of we routes
+// File of we routes
 const mainRoutes = require('./routes/mainRoutes.js');
 
-//View Template
+// View Template
 app.set('view engine', 'ejs');
 
-//Set mainRoutes with file of routes
+// Set mainRoutes with file of routes
 app.use('/', mainRoutes);
+
+// 404 Error Define
+app.use((req, res, next) => {
+    res.status(404).render('not-found');
+});
 
 //Listening Server
 app.listen(port, () => {
