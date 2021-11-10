d464de3c3f58e88891f6731a64909b5f797dde51
LF236 <fenandorodiguez236@gmail.com>
2021-11-09 20:54:30 -0600

    Se modifica el formulario y se inicia la estructura de multer

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const mainController = require('../controllers/mainController.js');
 const multer = require('multer');
+const fs = require('fs');
+const { v4: uuid } = require('uuid');
+const path = require('path');
+const mainController = require('../controllers/mainController.js');
 
 // Config files update
 const storage = multer.diskStorage({
-    
+    destination: (req, file, callback) => {
+        const id_user = uuid();
+        req.body['id_user'] = id_user;
+        const path_image_user = path.join(__dirname + '/../public/img/profile_images/' + id_user);
+        if(!fs.existsSync(path_image_user)) {
+            fs.mkdirSync(path_image_user);
+        }
+        callback(null, path_image_user);
+    },
+
+    filename: (req, file, callback) => {
+        const id_user = req.body.id_user;
+        callback(null, `perfil_${id_user}`);
+    }
 });
 // Routes
 router.get('/', mainController.home);
