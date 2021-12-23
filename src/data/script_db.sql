f70a3eb87dd5ab9c9b973dc04d27800552836077
SElyg <elisa.gomezhdz@hotmail.com>
2021-12-23 00:08:52 -0600

    BD

--- src/data/script_db.sql
+++ src/data/script_db.sql
@@ -10,7 +10,7 @@ CREATE TABLE Category(id varchar(36) NOT NULL, PRIMARY KEY(id),name varchar(50))
 
 CREATE TABLE Product_categories(id varchar(36) NOT NULL, PRIMARY KEY(id), product_id varchar(36) NOT NULL, FOREIGN KEY(product_id) REFERENCES Products(id), category_id varchar(36) NOT NULL, FOREIGN KEY(category_id) REFERENCES Category(id));
 
-CREATE TABLE User(id varchar(36) NOT NULL, PRIMARY KEY(id) ,email varchar(100) NOT NULL, password varchar(50) NOT NULL, name varchar(50) NOT NULL, last_name varchar(50) NOT NULL, image TEXT NOT NULL, role BOOLEAN DEFAULT false);
+CREATE TABLE User(id varchar(36) NOT NULL, PRIMARY KEY(id) ,email varchar(100) NOT NULL, password varchar(100) NOT NULL, name varchar(50) NOT NULL, last_name varchar(50) NOT NULL, image TEXT NOT NULL, role BOOLEAN DEFAULT false);
 
 CREATE TABLE Purchase(id varchar(36) NOT NULL, PRIMARY KEY(id), userId varchar(36) NOT NULL, FOREIGN KEY(userId) REFERENCES User(id), total_price INT NOT NULL, tracking_number INT NOT NULL, shipping_address TEXT, order_date datetime NOT NULL, order_status enum('PENDIENTE','ENVIADO','ENTREGADO','CANCELADO'));
 
@@ -19,4 +19,3 @@ CREATE TABLE Purchase_Details(id varchar(36) NOT NULL, purchase_id varchar(36) N
 CREATE TABLE Product_User(id varchar(36) NOT NULL, PRIMARY KEY(id), product_id varchar(36) NOT NULL, category_id varchar(36) NOT NULL, FOREIGN KEY(product_id) REFERENCES Products(id), FOREIGN KEY(category_id) REFERENCES Category(id));
 
 CREATE TABLE Reviews(id varchar(36) NOT NULL, PRIMARY KEY(id), title varchar(50), rating FLOAT NOT NULL, review TEXT, product_id varchar(36), FOREIGN KEY(product_id) REFERENCES Products(id), userId varchar(36), FOREIGN KEY(userId) REFERENCES User(id));
-
