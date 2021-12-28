5fcd2db7c8bc8bc56ffb43a18405b16981ad932c
SElyg <elisa.gomezhdz@hotmail.com>
2021-12-28 16:54:35 -0600

    seque.models

---
+++ src/database/models/Purchase.js
@@ -0,0 +1,54 @@
+module.exports = (sequelize, DataTypes) => {
+    const cols = {
+        id: {
+            primaryKey: true,
+            type: DataTypes.UUID,
+            allowNull: false
+        },
+
+        userId: {
+            type: DataTypes.UUID,
+            allowNull: false
+        },
+
+        total_price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+
+        tracking_number: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+
+        shipping_address: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+        
+        descImgI: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        },
+
+        order_date: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+
+        order_status: {
+            type: DataTypes.ENUM,
+            allowNull: true
+        }
+
+    };
+
+    const config = {
+        tableName: 'Purchase',
+        timestamps: false
+    };
+
+    const Purchase = sequelize.define("Purchase", cols, config);
+
+    return Purchase;
+}
